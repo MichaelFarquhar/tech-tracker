faa5a452e7d29c9d7f46c55c77d937aca2270898
Michael <michaelfarquhar4@gmail.com>
2021-02-08 00:44:07 -0500

    Installed helmetjs

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
+app.use(helmet());
+app.use(cors());
 
-app.get('/', cors(), (req, res) => {
-    return 'Hello';
+app.get('/', (req, res) => {
+    res.json('hio');
 });
 
 const PORT = 5000 | process.env.PORT;
