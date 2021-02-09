9cc5f61d8e0aed761c17c653aa0b300e3ffadf7d
Michael <michaelfarquhar4@gmail.com>
2021-02-08 23:07:49 -0500

    Added basic header

--- client/src/components/sections/Header.tsx
+++ client/src/components/sections/Header.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Container from '../utils/Container';
+import github from '../../svg/github.svg';
 
 function Header() {
     return (
-        <div>
-            <h1>Head</h1>
-        </div>
+        <header>
+            <Container>
+                <div className="header">
+                    <h1>Tech Tracker</h1>
+                    <a
+                        href="https://github.com/MichaelFarquhar/tech-tracker"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={github} alt="Github icon" width={35} height={35} />
+                    </a>
+                </div>
+            </Container>
+        </header>
     );
 }
 
